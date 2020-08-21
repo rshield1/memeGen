3f94140631fff9cae2b94bbbe20dbc1ed5c2cb15
Robert Shields <robert.shieldsiv@gmail.com>
2020-08-21 15:52:46 -0400

    Updates

---
+++ precache-manifest.8432ed7d850c3f3ccad7d07ad7393acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28157b75ba6033244c00898828f968da",
+    "url": "/memegen/index.html"
+  },
+  {
+    "revision": "bab684060911ae46c9a8",
+    "url": "/memegen/static/css/main.44e0ac16.chunk.css"
+  },
+  {
+    "revision": "0999d58fc5799e057b9e",
+    "url": "/memegen/static/js/2.e1fb9f6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memegen/static/js/2.e1fb9f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab684060911ae46c9a8",
+    "url": "/memegen/static/js/main.6a79b6bf.chunk.js"
+  },
+  {
+    "revision": "1f13fb36fa0a10d4bfc1",
+    "url": "/memegen/static/js/runtime-main.47c97c6c.js"
+  }
+]);
