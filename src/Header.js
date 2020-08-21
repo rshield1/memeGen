65f4d86331c762573313bdc2388a25eae1991d60
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-08-21 15:27:49 -0400

    added fetch request to api during mount stage

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,13 @@ import React from "react";
 
 function Header() {
     return (
-        <h1>Header</h1>
+        <header>
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+            alt="troll"/>
+            <p>Meme Generator</p>
+
+
+        </header>
     )
 }
 
--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -4,12 +4,48 @@ import React, { Component } from "react";
 class MemeGenerator extends Component {
     constructor() {
         super()
-        this.state = {}
+        // Initialize state
+        this.state = {
+            topText: "",
+            bottomText: '',
+            randomImg: 'http://i.imgflip.com/1bij.jpg',
+            allMemeImgs: []
+        }
     }
+
+    // Fetch data from api to frontend at the beginning
+
+    componentDidMount() {
+        fetch('https://api.imgflip.com/get_memes')
+        .then(res => res.json())
+        .then(response => {
+            const { memes } = response.data
+            console.log(memes[0])
+            this.setState({ allMemeImgs: memes })
+        })
+    }
+
 
     render() {
         return (
-            <h1>Meme Generator</h1>
+            <div>
+                <form className="meme-form">
+                <input 
+                    type="text" 
+                    name="topText" 
+                    placeholder="Top Text" 
+                    value={this.state.topText}
+                    />
+                <input 
+                    type="text" 
+                    name="bottomText" 
+                    placeholder="Bottom Text" 
+                    value={this.state.bottomText}
+                    />
+
+                    <button>Generate</button>
+                </form>
+            </div>
         )
     }
 }
