e8063c13fd80031f6c10dad92e71a6f05efb9352
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-08-21 15:03:18 -0400

    added meme generator components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
+import MemeGenerator from "./MemeGenerator";
+
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header/>
+      <MemeGenerator/>
     </div>
   );
 }
